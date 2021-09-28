0987905dfed7f37f68e23091ae6b366fd442b91d
Akita Emmanuel <e.akita91@gmail.com>
2021-09-28 01:05:06 +0100

    Added(): quiz card comp.

---
+++ src/components/QuizCard.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  makeStyles,
+  createStyles
+} from "@material-ui/core";
+
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+
+  })
+)
+
+export const QuizCard: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <Card>
+
+    </Card>
+  )
+}
