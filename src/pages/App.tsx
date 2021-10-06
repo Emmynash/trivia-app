7ae04f1b9e6fe54b6d969b467d4eae88ace07dfa
Akita Emmanuel <e.akita91@gmail.com>
2021-10-06 20:43:28 +0100

    update(): delete redundant code

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,34 +1,21 @@
 import React from "react";
-import { ProvideSideBarContext, ProvideThemeContext } from "hooks";
-import { Home } from "pages";
-import { Route, Router, Switch } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { ProvideSideBarContext, ProvideThemeContext, } from "hooks";
+import { Home, Quiz } from "pages";
+import { Route, BrowserRouter, Switch } from "react-router-dom";
 import { Layout } from "components";
-import ReactGA from "react-ga";
-
-const history = createBrowserHistory();
-
-if (process.env.NODE_ENV === 'production') {
-  ReactGA.initialize("UA-000000-01");
-  ReactGA.pageview(window.location.pathname + window.location.search);
-  history.listen(location => {
-    ReactGA.set({ page: location.location.pathname })
-    ReactGA.pageview(location.location.pathname)
-  })
-}
-
 export const App = () => {
   return (
-    <Router history={history}>
+    <BrowserRouter>
       <ProvideThemeContext>
         <ProvideSideBarContext>
           <Layout>
             <Switch>
-              <Route exact component={Home} path="/" />
+              <Route path='/' component={Home} exact />
+              <Route path="/quiz" component={Quiz} exact />
             </Switch>
           </Layout>
         </ProvideSideBarContext>
       </ProvideThemeContext>
-    </Router>
+    </BrowserRouter>
   )
 }
