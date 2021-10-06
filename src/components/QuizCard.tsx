fcb094e6f91c925930f0b2bf329b19eb1792b16a
Akita Emmanuel <e.akita91@gmail.com>
2021-10-06 20:40:35 +0100

    update(): delete quiz card

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -3,22 +3,43 @@ import {
   Card,
   CardContent,
   makeStyles,
-  createStyles
+  createStyles,
+  Typography
 } from "@material-ui/core";
 
 
 const useStyles = makeStyles((theme) =>
   createStyles({
-
+    card: {
+      maxWidth: 300,
+      margin: "auto",
+      transition: "0.3s",
+      boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+      "&:hover": {
+        boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+      }
+    },
   })
 )
-
-export const QuizCard: React.FC = () => {
+interface Props {
+  quiz: []
+}
+export const QuizCard: React.FC<Props> = ({ quiz }) => {
   const classes = useStyles();
 
   return (
-    <Card>
-
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography
+          variant="h2"
+          gutterBottom
+        >
+          {/* {quiz.category} */}
+        </Typography>
+        <Typography variant={"caption"}>
+          {/* {quiz.question} */}
+        </Typography>
+      </CardContent>
     </Card>
   )
 }
